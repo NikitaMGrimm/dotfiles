4c968d6c6a61c8447264e46f85cfb10c3ca82039
Nikita Maximilian Grimm <nikitagrimm03@gmail.com>
2023-05-01 21:56:35 +0200

    clean up, small changes

--- home/dot_config/nvim/lua/exact_custom/chadrc.lua
+++ home/dot_config/nvim/lua/exact_custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "yoru",
-  theme_toggle = { "yoru", "one_light" },
+  theme = "gruvbox",
+  theme_toggle = { "gruvbox", "one_light" },
   transparency = true,
 
   hl_override = highlights.override,
